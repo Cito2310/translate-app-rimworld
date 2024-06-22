aca549008dc6a402be460d5aa37bdd3ac5ac40ba
Cito2310 <cito2310@gmail.com>
2024-06-22 08:03:22 -0300

    finish version 2.0 to translateAppRimworld

--- src/SectionTranslate/SectionTranslate.tsx
+++ src/SectionTranslate/SectionTranslate.tsx
@@ -20,17 +20,17 @@ export const SectionTranslate = ({ control, getValues }: props) => {
     return (
         <div className="bg-[#1f1f1f] grow text-[#ddd] px-4 overflow-auto pb-10">
             {
-                defInjectedShow.length && <>
+                defInjectedShow.length ? <>
                     <TextMain text="DefInjected" />
                     {defInjectedShow.map( data => <DefInjectedRow key={data.name} getValues={getValues} control={control} data={data} /> )}
-                </>
+                </> : null
             }
 
             {
-                keyed.length && <>
+                keyed.length ? <>
                     <TextMain text="Keyed" />
                     {keyed.map( data => <KeyedRow key={data.name}  getValues={getValues} control={control} data={data} /> )}
-                </>
+                </> : null
             }
         </div>
     )
